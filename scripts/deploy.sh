ca5c4976cf62ce4d2674477633789f851f2e1a5c
Henri Meltaus <henri.meltaus@webscale.fi>
2022-07-14 15:47:54 +0300

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,11 +20,13 @@ build_and_deploy() {
   cd build
 
   aws s3 cp . s3://takomo-website-bucket${BASE_URL} \
+    --region us-east-1 \
     --recursive \
     --exclude "*.html" \
     --cache-control "public, max-age=604800"
 
   aws s3 cp . s3://takomo-website-bucket${BASE_URL} \
+    --region us-east-1 \
     --recursive \
     --exclude "*" \
     --include "*.html" \
