39a21ab1f35f99a7ccaed1d3574aeba0d9ff4a41
Henri Meltaus <henri.meltaus@webscale.fi>
2026-01-26 10:31:55 +0200

    release 9.0.2

--- rspress.config.ts
+++ rspress.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     },
   },
   title: 'Takomo',
-  logoText: 'Takomo v9.0.1',
+  logoText: 'Takomo v9.0.2',
   description: 'Organize, parameterize and deploy your CloudFormation stacks',
   icon: '/takomo-icon.png',
   logo: {
