920cf1f4f835f7ebeb84786d4a6586fc32f94b9d
Henri Meltaus <henri.meltaus@webscale.fi>
2026-04-18 20:14:42 +0300

    release 10.0.2

--- components/ApiLink.tsx
+++ components/ApiLink.tsx
@@ -6,7 +6,7 @@ export default (props: { text: string; source: string }) => {
   // }
 
   // const apiVersion = process.env.TAKOMO_VERSION!.replace(/\./g, '-')
-  const apiVersion = '10-0-1';
+  const apiVersion = '10-0-2';
 
   return (
     <a
--- rspress.config.ts
+++ rspress.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     },
   },
   title: 'Takomo',
-  logoText: 'Takomo v10.0.1',
+  logoText: 'Takomo v10.0.2',
   description: 'Organize, parameterize and deploy your CloudFormation stacks',
   icon: '/takomo-icon.png',
   logo: {
